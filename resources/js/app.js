54b9f9a7a951739d75e166ef5b4029bdc221f3b0
aespinozacortez <aespinozacortez04@gmail.com>
2024-04-15 01:32:30 +0200

    ckeditor + daisy ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,13 @@
 import './bootstrap';
 import '../css/app.css';
+import '../css/ckeditor-reset.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import CKEditor from '@ckeditor/ckeditor5-vue'
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -15,6 +18,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(CKEditor)
             .mount(el);
     },
     progress: {
